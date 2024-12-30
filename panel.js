1216370dbc3689ab1161d798f52b549974817a98
Swabhan Katkoori <swabhankatkoori@gmail.com>
2024-12-30 18:02:17 -0500

    Display Current Path to Side Panel

--- panel.js
+++ panel.js
@@ -12,6 +12,36 @@
 */
 chrome.runtime.sendMessage("panel_data", (response) => { //- Refer to service.js
     console.log(response["path"]);
-});
 
-const header = document.getElementById("header");
+    const pathLength = response["path"].length;
+
+    for (var i = 0; i < pathLength; i++) { 
+        //Connect to List UI and create new item
+        var ul = document.getElementById("linked-list");
+        var li = document.createElement("li");
+
+        //Create Node
+        var divNode = document.createElement("div");
+        var nodeText = document.createTextNode("1");
+        divNode.appendChild(nodeText)
+        divNode.className = "node";
+
+        //Create Content
+        var divContent = document.createElement("div");
+        const contentText = document.createTextNode(response["path"][i]);
+        var contentP = document.createElement("p");
+        contentP.appendChild(contentText);
+        divContent.appendChild(contentP);
+        divContent.className = "content";
+        
+        //Add node and content to List Item
+        li.appendChild(divNode);
+        li.appendChild(divContent);
+
+        //Add list item to UL
+        ul.appendChild(li);
+    
+    }
+
+    
+});
